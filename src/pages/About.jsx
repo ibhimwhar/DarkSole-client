f0be5449386a91dd5826fced146f4ac481add9b5
ibrahim <adjeiibrahim2008@gmail.com>
2025-05-24 17:31:26 -0400

    Added Text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,11 @@ const About = () => {
     return (
         <section className="min-h-screen flex flex-col justify-center px-6 py-20 text-center bg-gray-900">
             <h2 className="text-3xl font-bold text-indigo-400 mb-6">About DarkSole</h2>
+            <p className="text-gray-400 max-w-xl mx-auto mb-6">
+                Just a project website
+            </p>
             <p className="text-gray-400 max-w-xl mx-auto">
-                DarkSole is dedicated to designing high-quality, stylish, and durable footwear that fits every lifestyle. From city streets to rugged trails, we've got your soles covered.
+                I'm a self-taught web developer with a passion for building clean, responsive, and user-friendly websites and apps. I enjoy turning ideas into digital experiences using modern tools like React, Tailwind CSS, and Node.js. Always eager to learn and grow, I'm constantly exploring new technologies and best practices to improve my craft and build impactful projects.
             </p>
         </section>
     )
